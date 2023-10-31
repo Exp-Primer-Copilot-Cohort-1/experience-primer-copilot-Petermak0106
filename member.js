32b3b3c70e5995bc7e05043381559a920f2b2274
Petermak0106 <61486601+Petermak0106@users.noreply.github.com>
2023-10-31 04:39:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skill = document.getElementById("skill");
+    var about = document.getElementById("about");
+    var project = document.getElementById("project");
+    var contact = document.getElementById("contact");
+
+    member.style.display = "block";
+    skill.style.display = "none";
+    about.style.display = "none";
+    project.style.display = "none";
+    contact.style.display = "none";
+}
